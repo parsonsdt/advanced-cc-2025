2a1bd0186e085637a9736c786419a382d0bb5b67
gabedeko <gabe.deko@gmail.com>
2025-03-14 20:38:13 -0400

    add local storage example form

---
+++ class_examples/local_storage_form/two.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    document.querySelector("#name").innerHTML = localStorage.getItem("name")
+    document.querySelector("#text").style.background = localStorage.getItem("color")
+
+
+})
