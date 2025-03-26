96db34f6bf8396dae104f036179abe6405710533
gabedeko <gabe.deko@gmail.com>
2025-03-25 21:37:45 -0400

    css updates for spring 2025

--- class_examples/local_storage_form/script.js
+++ class_examples/local_storage_form/script.js
@@ -1,15 +1,49 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     document.querySelector("#name").addEventListener("keyup",function(){
-
+        let val = this.value;
+        localStorage.setItem("name", val) 
     })
 
     let radios = document.querySelectorAll('.radio'); 
 
-    for (var i = radios.length - 1; i >= 0; i--) {
+    for (let i = radios.length - 1; i >= 0; i--) {
        radios[i].addEventListener('change',function(){
             localStorage.setItem("color", this.value)
         })
     }
+
+    let fontOption = document.querySelector('#fontOption');
+
+    fontOption.addEventListener('change',function(){
+        localStorage.setItem("font", this.value)
+    })
+
+    let spacingOptions = document.querySelectorAll('.spacing-radio');
+
+    for (let i = spacingOptions.length - 1; i >= 0; i--) {
+        spacingOptions[i].addEventListener('change',function(){
+            localStorage.setItem("spacing", this.value)
+        })
+    }
+
+    let buttonOption = document.querySelector('#buttonOption');
+
+    buttonOption.addEventListener('change',function(){
+        localStorage.setItem("button", this.value)
+    })
+
+    let randomMessage = document.querySelector('#randomMsg');
+
+    randomMessage.addEventListener('change', function() {
+        if(randomMessage.checked){
+            console.log("checked");
+            localStorage.setItem("message", "checked");
+        } else {
+            console.log("unchecked");
+            localStorage.setItem("message", "unchecked");
+        }
+      });
+
 
 })
--- class_examples/local_storage_form/two.js
+++ class_examples/local_storage_form/two.js
@@ -1,7 +1,68 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    document.querySelector("#name").innerHTML = localStorage.getItem("name")
-    document.querySelector("#text").style.background = localStorage.getItem("color")
+    document.querySelector("#nameOption").innerHTML = localStorage.getItem("name");
+    //document.querySelector("#text").style.background = localStorage.getItem("color");
+
+    document.body.classList.add(localStorage.getItem("color"));
+    document.querySelector("#mainContainer").classList.add(localStorage.getItem("color"));
+    document.querySelector("#buttonOption").classList.add(localStorage.getItem("color"));
+
+    if (localStorage.getItem("font") == "audiowide") {
+        document.querySelector("#textOption").classList.add("audiowide-regular");
+        document.querySelector("#messageOption").classList.add("audiowide-regular");
+        document.querySelector("#buttonOption").classList.add("audiowide-regular");
+        
+    } else if (localStorage.getItem("font") == "amarante") {
+        document.querySelector("#textOption").classList.add("amarante-regular");
+        document.querySelector("#messageOption").classList.add("amarante-regular");
+        document.querySelector("#buttonOption").classList.add("amarante-regular");
+    } else {
+        document.querySelector("#textOption").classList.add("monoton-regular");
+        document.querySelector("#messageOption").classList.add("monoton-regular");
+        document.querySelector("#buttonOption").classList.add("monoton-regular");
+    }
+
+    console.log(localStorage.getItem("message"));
+    if (localStorage.getItem("message") == "checked") {
+        document.querySelector("#messageOption").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tempor libero. Etiam vitae mollis leo, nec mollis enim. Mauris euismod, augue et pulvinar vulputate, enim est congue leo, in condimentum sem magna a ante. Integer sapien massa, posuere eget ante ut, cursus suscipit ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum quis ante purus. Nam elit nisi, viverra luctus ipsum ut, convallis ullamcorper erat. Mauris ultrices ut velit vel finibus. Vivamus efficitur sodales risus, commodo pharetra sem mattis nec. In eget nulla sit amet erat mattis pulvinar in quis augue. Etiam venenatis interdum tellus, vitae semper elit euismod mollis. Etiam suscipit imperdiet orci. Proin consequat lectus ligula, id fringilla elit aliquam et. Ut dictum quis est vehicula vestibulum. z";
+    } else {
+        document.querySelector("#messageOption").innerHTML = "You left the message box unchecked :/";
+    }
+        
+
+    if (localStorage.getItem("spacing") == "low_space") {
+        document.querySelector("#textOption").classList.add("smallSpacing");
+        document.querySelector("#messageOption").classList.add("smallSpacing");
+        document.querySelector("#mainContainer").classList.add("smallSpacing");
+    } else if (localStorage.getItem("spacing") == "mid_space") {
+        document.querySelector("#textOption").classList.add("mediumSpacing");
+        document.querySelector("#messageOption").classList.add("mediumSpacing");
+        document.querySelector("#mainContainer").classList.add("mediumSpacing");
+    } else {
+        document.querySelector("#textOption").classList.add("largeSpacing");
+        document.querySelector("#messageOption").classList.add("largeSpacing");
+        document.querySelector("#mainContainer").classList.add("largeSpacing");
+    }
+
+    if (localStorage.getItem("button") == "round") {
+        document.querySelector("#buttonOption").classList.add("rounded");
+    } else if (localStorage.getItem("button") == "standard") {
+        document.querySelector("#buttonOption").classList.add("standard");
+     }else {
+        document.querySelector("#buttonOption").classList.add("dashed");
+    }
+
+    
+
+
+
+    console.log("Name: " + localStorage.getItem("name"));
+    console.log("Color: " + localStorage.getItem("color"));
+    console.log("Font: " + localStorage.getItem("font"));
+    console.log("Spacing: " + localStorage.getItem("spacing"));
+    console.log("Button: " + localStorage.getItem("button"));
+    console.log("Message: " + localStorage.getItem("message"));
+
 
 
 })
