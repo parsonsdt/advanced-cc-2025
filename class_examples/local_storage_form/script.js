42ac6fa437bb409e924ca9957c5565bfcd84d0c6
gabedeko <gabe.deko@gmail.com>
2025-03-25 22:45:18 -0400

    clean upp and add comments

--- class_examples/local_storage_form/script.js
+++ class_examples/local_storage_form/script.js
@@ -1,49 +1,50 @@
-document.addEventListener("DOMContentLoaded",function(){
+// Wait for the DOM to fully load before executing the script
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector("#name").addEventListener("keyup",function(){
+    // Save the value of the "name" input field to localStorage on keyup
+    document.querySelector("#name").addEventListener("keyup", function () {
         let val = this.value;
-        localStorage.setItem("name", val) 
-    })
+        localStorage.setItem("name", val);
+    });
 
-    let radios = document.querySelectorAll('.radio'); 
+    // Save the selected color (radio button) to localStorage
+    let radios = document.querySelectorAll('.radio');
+    radios.forEach(radio => {
+        radio.addEventListener('change', function () {
+            localStorage.setItem("color", this.value);
+        });
+    });
 
-    for (let i = radios.length - 1; i >= 0; i--) {
-       radios[i].addEventListener('change',function(){
-            localStorage.setItem("color", this.value)
-        })
-    }
-
+    // Save the selected font option to localStorage
     let fontOption = document.querySelector('#fontOption');
+    fontOption.addEventListener('change', function () {
+        localStorage.setItem("font", this.value);
+    });
 
-    fontOption.addEventListener('change',function(){
-        localStorage.setItem("font", this.value)
-    })
-
+    // Save the selected spacing option (radio buttons) to localStorage
     let spacingOptions = document.querySelectorAll('.spacing-radio');
+    spacingOptions.forEach(option => {
+        option.addEventListener('change', function () {
+            localStorage.setItem("spacing", this.value);
+        });
+    });
 
-    for (let i = spacingOptions.length - 1; i >= 0; i--) {
-        spacingOptions[i].addEventListener('change',function(){
-            localStorage.setItem("spacing", this.value)
-        })
-    }
-
+    // Save the selected button style to localStorage
     let buttonOption = document.querySelector('#buttonOption');
+    buttonOption.addEventListener('change', function () {
+        localStorage.setItem("button", this.value);
+    });
 
-    buttonOption.addEventListener('change',function(){
-        localStorage.setItem("button", this.value)
-    })
-
+    // Save the state of the "random message" checkbox to localStorage
     let randomMessage = document.querySelector('#randomMsg');
-
-    randomMessage.addEventListener('change', function() {
-        if(randomMessage.checked){
+    randomMessage.addEventListener('change', function () {
+        if (randomMessage.checked) {
             console.log("checked");
             localStorage.setItem("message", "checked");
         } else {
             console.log("unchecked");
             localStorage.setItem("message", "unchecked");
         }
-      });
+    });
 
-
-})
+});
