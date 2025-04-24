2e51118d0ab73e628a526acc169e5543494af452
gabedeko <gabe.deko@gmail.com>
2025-04-24 14:50:55 -0400

    modify api await/async example

--- class_examples/api_await_async_ex/script.js
+++ class_examples/api_await_async_ex/script.js
@@ -1,42 +1,51 @@
-// document.addEventListener('DOMContentLoaded', function () {
-// });
+
 
 document.addEventListener('DOMContentLoaded', fetchArtData);
 
 async function fetchArtData() {
     
-    let randArt = Math.floor(Math.random() * 1000) + 1;
-    console.log(randArt);
     try {
-        
-        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randArt}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&units=metric&appid=bc0dffe988a5874f093f027fbf71411f`)
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-
         const data = await response.json();
-        displayArt(data);
+        showData(data);
+        
     }
-
     catch(error){
         console.error('Fetch error:', error);
     };
 };
 
-function displayArt(art) {
-    console.log(art);
-    const artList = document.getElementById('artList');
-    const artItem = document.createElement('div');
+function showData(data){
+    console.log(data);
+    console.log(data.weather[0].description);
+    //Create your front end here
 
-    artItem.innerHTML = `
-        <h2>${art.title}</h2>
-        <img src="${art.primaryImageSmall}" alt="${art.title}" />
-        <p>Artist: ${art.artistDisplayName}</p>
-        <p>Year: ${art.objectDate}</p>
-        <p>Credit Line: ${art.creditLine}</p>
+    //convert temp from celcius to farenheit
+    let temp_cel = data.main.temp;
+    let temp_fah = (temp_cel * 9/5) + 32;
+
+    //get the weather description and location
+    let weather_desc = data.weather[0].description;
+    let location = data.name;
+
+    //get element in html and append data
+    const weatherSection = document.getElementById('weatherSection');
+    const weatherItem = document.createElement('div');
+
+    weatherItem.innerHTML = `
+        <hr>
+        <ul>
+        <li>The location is ${location}</li>
+        <li>The forecast is ${weather_desc}</li>
+        <li>The temperature in farenheit is ${temp_fah.toFixed(2)} degrees</li>
+        <ul>
+        <p> </p>
     `;
 
-    artList.appendChild(artItem);
+    weatherSection.appendChild(weatherItem);
 
 }
